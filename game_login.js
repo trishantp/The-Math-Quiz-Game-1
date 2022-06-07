5b5f817bcee58c6c4264e2fb88a78a6954876ba3
trishantp <99416253+trishantp@users.noreply.github.com>
2022-06-07 17:32:30 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,15 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    //Set "player1_name" using localStorage.setItem() function
+    localStorage.setItem("player_1",player1_name);
+
+	  //Set "player2_name" using localStorage.setItem() function
+    localStorage.setItem("player_2",player2_name);
+
+
+    window.location = "game_page.html";
+}
+
